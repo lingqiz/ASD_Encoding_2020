d59d06cf727ce1a6d8c8c2c72c7f2ea1a2a2dabd
lingqiz <zlqzcc@gmail.com>
2020-07-05 22:22:25 -0400

    add corr scq

--- Figure_5_S4/fig5_sub_corr.m
+++ Figure_5_S4/fig5_sub_corr.m
@@ -1,38 +1,40 @@
 %% Add path
 addpath('./cbrewer/');
 addpath('./CircStat/');
-addpath('./Figure_5/');
+addpath('./Figure_5_S4/');
 
 %% Prepare data for correlation analysis
 [prior_td, noise_td]   = collectFit('./woFB/TD/*.mat', './wFB1/TD/*.mat', './wFB2/TD/*.mat', 25);
 [prior_asd, noise_asd] = collectFit('./woFB/ASD/*.mat', './wFB1/ASD/*.mat', './wFB2/ASD/*.mat', 17);
 
-%% Plot figures
+%% Plot figures 5
 % Note for convenience, we saved all the paramters extracted with the function above 
 % together with the clinical data in the .mat file below
 load('para_clinical.mat');
+scoreID = 5;
+
 figure(); 
 set(gcf,'Position',[0, 0, 720, 1080]);
 
 %% prior woFB
 subplot(3, 2, 5);
-sessionID = 2; % Prior before learning
-corrPlotPrior(prior_td, prior_asd, sessionID, 'AQ', 'Prior woFB', [0.0, 1.0], 0 : 0.2 : 1);
+sessionID = 2; % Before learning
+corrPlotPrior(prior_td, prior_asd, sessionID, scoreID, 'AQ', 'Prior woFB', [0.0, 1.0], 0 : 0.2 : 1);
 
 %% prior wFB2
 subplot(3, 2, 6);
-sessionID = 4; % Prior before learning
-corrPlotPrior(prior_td, prior_asd, sessionID, 'AQ', 'Prior wFB2', [0.0, 1.0], 0 : 0.2 : 1);
+sessionID = 4; % After learning
+corrPlotPrior(prior_td, prior_asd, sessionID, scoreID, 'AQ', 'Prior wFB2', [0.0, 1.0], 0 : 0.2 : 1);
 
 %% FI woFB
 subplot(3, 2, 3);
-sessionID = 2; % Prior before learning
-corrPlotFI(noise_td, noise_asd, sessionID, 'AQ', 'FI woFB', [5, 30], 5 : 5 : 30);
+sessionID = 2; % Before learning
+corrPlotFI(noise_td, noise_asd, sessionID, scoreID, 'AQ', 'FI woFB', [5, 30], 5 : 5 : 30);
 
 %% FI wFB2
 subplot(3, 2, 4);
-sessionID = 4; % Prior before learning
-corrPlotFI(noise_td, noise_asd, sessionID, 'AQ', 'FI wFB2', [5, 30], 5 : 5 : 30);
+sessionID = 4; % After learning
+corrPlotFI(noise_td, noise_asd, sessionID, scoreID, 'AQ', 'FI wFB2', [5, 30], 5 : 5 : 30);
 
 %% Correlation, FI before feedback with prior
 subplot(3, 2, 1);
@@ -54,8 +56,37 @@ line = plot(xlim(), xlim() * lm.Coefficients.Estimate(2) + lm.Coefficients.Estim
 ylim([0, 1]);
 yticks(0 : 0.2 : 1);
 
+%% Plot figures S4
+% Note for convenience, we saved all the paramters extracted with the function above 
+% together with the clinical data in the .mat file below
+load('para_clinical.mat');
+scoreID = 6;
+
+figure(); 
+set(gcf,'Position',[0, 0, 720, 1080]);
+
+%% prior woFB
+subplot(2, 2, 3);
+sessionID = 2; % Before learning
+corrPlotPrior(prior_td, prior_asd, sessionID, scoreID, 'SCQ', 'Prior woFB', [0.0, 1.0], 0 : 0.2 : 1);
+
+%% prior wFB2
+subplot(2, 2, 4);
+sessionID = 4; % After learning
+corrPlotPrior(prior_td, prior_asd, sessionID, scoreID, 'SCQ', 'Prior wFB2', [0.0, 1.0], 0 : 0.2 : 1);
+
+%% FI woFB
+subplot(2, 2, 1);
+sessionID = 2; % Before learning
+corrPlotFI(noise_td, noise_asd, sessionID, scoreID, 'SCQ', 'FI woFB', [5, 30], 5 : 5 : 30);
+
+%% FI wFB2
+subplot(2, 2, 2);
+sessionID = 4; % After learning
+corrPlotFI(noise_td, noise_asd, sessionID, scoreID, 'SCQ', 'FI wFB2', [5, 30], 5 : 5 : 30);
+
 %% Helper functions
-function corrPlotPrior(td, asd, id, xStr, yStr, ylimArray, ytickArray)
+function corrPlotPrior(td, asd, sID, cID, xStr, yStr, ylimArray, ytickArray)
 
 colormap = cbrewer('seq', 'YlGnBu', 9);
 color_td = colormap(7, :);
@@ -63,13 +94,13 @@ color_td = colormap(7, :);
 colormap = cbrewer('seq', 'YlOrRd', 9);
 color_asd = colormap(7, :);
 
-scatter(td(:, 5), td(:, id), 40, color_td, 'filled');
+scatter(td(:, cID), td(:, sID), 40, color_td, 'filled');
 hold on;
-scatter(asd(:, 5), asd(:, id), 40, color_asd, 'filled');
+scatter(asd(:, cID), asd(:, sID), 40, color_asd, 'filled');
 xlabel(xStr);
 ylabel(yStr);
 
-lm = fitlm([td(:, 5); asd(:, 5)], [td(:, id); asd(:, id)], ...
+lm = fitlm([td(:, cID); asd(:, cID)], [td(:, sID); asd(:, sID)], ...
     'linear', 'RobustOpts', 'on')
 % lm.Coefficients
 
@@ -77,18 +108,18 @@ line = plot(xlim(), xlim() * lm.Coefficients.Estimate(2) + lm.Coefficients.Estim
 ylim(ylimArray);
 yticks(ytickArray);
 
-errorbar(5, median(td(:, id)), std(td(:, id)) ...
-    / sqrt(length(td(:, id))), 'o', 'Color', color_td, 'LineWidth', 1.5);
+errorbar(min(xlim()) + 1, median(td(:, sID)), std(td(:, sID)) ...
+    / sqrt(length(td(:, sID))), 'o', 'Color', color_td, 'LineWidth', 1.5);
 
-errorbar(45, median(asd(:, id)), std(asd(:, id)) ...
-    / sqrt(length(asd(:, id))), 'o', 'Color', color_asd, 'LineWidth', 1.5);
+errorbar(max(xlim()) - 1, median(asd(:, sID)), std(asd(:, sID)) ...
+    / sqrt(length(asd(:, sID))), 'o', 'Color', color_asd, 'LineWidth', 1.5);
 
 ylim(ylimArray);
 yticks(ytickArray);
 
 end
 
-function corrPlotFI(td, asd, id, xStr, yStr, ylimArray, ytickArray)
+function corrPlotFI(td, asd, sID, cID, xStr, yStr, ylimArray, ytickArray)
 
 colormap = cbrewer('seq', 'YlGnBu', 9);
 color_td = colormap(7, :);
@@ -96,24 +127,24 @@ color_td = colormap(7, :);
 colormap = cbrewer('seq', 'YlOrRd', 9);
 color_asd = colormap(7, :);
 
-scatter(td(:, 5), td(:, id), 40, color_td, 'filled');
+scatter(td(:, cID), td(:, sID), 40, color_td, 'filled');
 hold on;
-scatter(asd(:, 5), asd(:, id), 40, color_asd, 'filled');
+scatter(asd(:, cID), asd(:, sID), 40, color_asd, 'filled');
 xlabel(xStr);
 ylabel(yStr);
 
-lm = fitlm([td(:, 5); asd(:, 5)], [td(:, id); asd(:, id)])
+lm = fitlm([td(:, cID); asd(:, cID)], [td(:, sID); asd(:, sID)])
 % lm.Coefficients
 
 line = plot(xlim(), xlim() * lm.Coefficients.Estimate(2) + lm.Coefficients.Estimate(1), '--k', 'LineWidth', 2);
 ylim(ylimArray);
 yticks(ytickArray);
 
-errorbar(5, median(td(:, id)), std(td(:, id)) ...
-    / sqrt(length(td(:, id))), 'o', 'Color', color_td, 'LineWidth', 1.5);
+errorbar(min(xlim()) + 1, median(td(:, sID)), std(td(:, sID)) ...
+    / sqrt(length(td(:, sID))), 'o', 'Color', color_td, 'LineWidth', 1.5);
 
-errorbar(45, median(asd(:, id)), std(asd(:, id)) ...
-    / sqrt(length(asd(:, id))), 'o', 'Color', color_asd, 'LineWidth', 1.5);
+errorbar(max(xlim()) - 1, median(asd(:, sID)), std(asd(:, sID)) ...
+    / sqrt(length(asd(:, sID))), 'o', 'Color', color_asd, 'LineWidth', 1.5);
 
 ylim(ylimArray);
 yticks(ytickArray);

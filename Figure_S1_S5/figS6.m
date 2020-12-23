024647cf4798da86da5dbafaadcc87ea72577ea7
lingqiz <zlqzcc@gmail.com>
2020-12-23 15:18:47 -0500

    add analysis

--- Figure_S1_S5/figS6.m
+++ Figure_S1_S5/figS6.m
@@ -37,9 +37,9 @@ xticks([0, 45, 90, 135, 180]);
 xlim([0, 180]);
 
 %% Search procedure
-para_woFB = fmincon(@(x) lossFunc(x, 0.59), 0.5, [], [], [], [], 0, 1);
-para_wFB1 = fmincon(@(x) lossFunc(x, 0.55), 0.5, [], [], [], [], 0, 1);
-para_wFB2 = fmincon(@(x) lossFunc(x, 0.51), 0.5, [], [], [], [], 0, 1);
+para_woFB = fmincon(@(x) lossFunc(x, 0.59, 16.75, 0.18), 0.5, [], [], [], [], 0, 1);
+para_wFB1 = fmincon(@(x) lossFunc(x, 0.55, 16.75, 0.18), 0.5, [], [], [], [], 0, 1);
+para_wFB2 = fmincon(@(x) lossFunc(x, 0.51, 16.75, 0.18), 0.5, [], [], [], [], 0, 1);
 
 figure(); subplot(1, 2, 1); hold on;
 errorbar(0.8, mean(scale_woFB_td, 2), std(scale_woFB_td, 0, 2), '--ob', 'LineWidth', 2);
@@ -86,18 +86,68 @@ plot([0.8, 2, 3.2], [16.75, 16.75, 16.75], '-k', 'LineWidth', 2);
 xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
 xticklabels({'woFB', 'wFB1', 'wFB2'});
 
+%% For control group
+para_woFB = fmincon(@(x) lossFunc(x, 0.54, 18.83, 0.085), 0.5, [], [], [], [], 0, 1);
+para_wFB1 = fmincon(@(x) lossFunc(x, 0.40, 18.83, 0.085), 0.5, [], [], [], [], 0, 1);
+para_wFB2 = fmincon(@(x) lossFunc(x, 0.34, 18.83, 0.085), 0.5, [], [], [], [], 0, 1);
+
+figure(); subplot(1, 2, 1); hold on;
+errorbar(0.8, mean(scale_woFB_td, 2), std(scale_woFB_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(2.0, mean(scale_wFB1_td, 2), std(scale_wFB1_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(3.2, mean(scale_wFB2_td, 2), std(scale_wFB2_td, 0, 2), '--ob', 'LineWidth', 2);
+
+plot([0.8, 2, 3.2], mean([scale_woFB_td; scale_wFB1_td; scale_wFB2_td], 2), '--b', 'LineWidth', 2);
+
+errorbar(0.8, mean(scale_woFB_asd, 2), std(scale_woFB_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(2.0, mean(scale_wFB1_asd, 2), std(scale_wFB1_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(3.2, mean(scale_wFB2_asd, 2), std(scale_wFB2_asd, 0, 2), '--or', 'LineWidth', 2);
+
+plot([0.8, 2, 3.2], mean([scale_woFB_asd; scale_wFB1_asd; scale_wFB2_asd], 2), '--r', 'LineWidth', 2);
+
+errorbar(0.8, para_woFB, std(scale_woFB_asd, 0, 2), '--ok', 'LineWidth', 2);
+errorbar(2.0, para_wFB1, std(scale_wFB1_asd, 0, 2), '--ok', 'LineWidth', 2);
+errorbar(3.2, para_wFB2, std(scale_wFB2_asd, 0, 2), '--ok', 'LineWidth', 2);
+
+plot([0.8, 2, 3.2], [para_woFB; para_wFB1; para_wFB2], '--k', 'LineWidth', 2);
+
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]); ylim([0.25, 0.8]);
+xticklabels({'woFB', 'wFB1', 'wFB2'});
+
+subplot(1, 2, 2); hold on;
+
+errorbar(0.8, mean(noise_woFB_td, 2), std(noise_woFB_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(2.0, mean(noise_wFB1_td, 2), std(noise_wFB1_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(3.2, mean(noise_wFB2_td, 2), std(noise_wFB2_td, 0, 2), '--ob', 'LineWidth', 2);
+
+l1 = plot([0.8, 2, 3.2], mean([noise_woFB_td; noise_wFB1_td; noise_wFB2_td], 2), '--b', 'LineWidth', 2);
+
+errorbar(0.8, mean(noise_woFB_asd, 2), std(noise_woFB_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(2.0, mean(noise_wFB1_asd, 2), std(noise_wFB1_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(3.2, mean(noise_wFB2_asd, 2), std(noise_wFB2_asd, 0, 2), '--or', 'LineWidth', 2);
+
+l2 = plot([0.8, 2, 3.2], mean([noise_woFB_asd; noise_wFB1_asd; noise_wFB2_asd], 2), '--r', 'LineWidth', 2);
+
+errorbar(0.8, 18.83, std(scale_woFB_asd, 0, 2), '-ok', 'LineWidth', 2);
+errorbar(2.0, 18.83, std(scale_wFB1_asd, 0, 2), '-ok', 'LineWidth', 2);
+errorbar(3.2, 18.83, std(scale_wFB2_asd, 0, 2), '-ok', 'LineWidth', 2);
+
+plot([0.8, 2, 3.2], [18.83, 18.83, 18.83], '-k', 'LineWidth', 2);
+
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
+xticklabels({'woFB', 'wFB1', 'wFB2'});
+
 %% Helper function
 
-function loss = lossFunc(priorNoNoise, priorData)
+function loss = lossFunc(priorNoNoise, priorData, totalFI, noiseRatio)
 
 stepSize = 0.01; 
 stmSpc = 0 : stepSize : 2 * pi;
 prior = priorHandle(priorNoNoise);
 
-FI = 16.75 * prior(stmSpc);
+FI = totalFI * prior(stmSpc);
 estVar = 1 ./ FI;
 
-noiseVar = estVar + 0.18 * mean(estVar);
+noiseVar = estVar + noiseRatio * mean(estVar);
 noiseFI = 1 ./ noiseVar;
 noisePrior = noiseFI ./ trapz(stmSpc, noiseFI);
 

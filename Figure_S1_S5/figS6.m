0fb6cfbb3f06a18bdb27b47519ef5d18c9fe6dd8
lingqiz <zlqzcc@gmail.com>
2021-01-21 10:49:11 -0500

    add figure

--- Figure_S1_S5/figS6.m
+++ Figure_S1_S5/figS6.m
@@ -1,9 +1,9 @@
 %% Change of parameter due to noise
 stepSize = 0.01; 
 stmSpc = 0 : stepSize : 2 * pi;
-prior = priorHandle(0.71);
+prior = priorHandle(0.51);
 
-FI = 16.5 * prior(stmSpc);
+FI = 14.8 * prior(stmSpc);
 estVar = 1 ./ FI;
 
 angle = stmSpc / (2 * pi) * 180;
@@ -17,7 +17,7 @@ subplot(1, 2, 2);
 plot(angle, FI, 'k', 'LineWidth', 2);
 trapz(stmSpc, FI)
 
-noiseVar = estVar + 0.18 * mean(estVar);
+noiseVar = estVar + 0.1 * mean(estVar);
 noiseFI = 1 ./ noiseVar;
 noisePrior = noiseFI ./ trapz(stmSpc, noiseFI);
 
@@ -83,6 +83,50 @@ errorbar(2.0, 16.75, std(scale_wFB1_asd, 0, 2), '-ok', 'LineWidth', 2);
 errorbar(3.2, 16.75, std(scale_wFB2_asd, 0, 2), '-ok', 'LineWidth', 2);
 
 l3 = plot([0.8, 2, 3.2], [16.75, 16.75, 16.75], '--k', 'LineWidth', 2);
+
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
+xticklabels({'woFB', 'wFB1', 'wFB2'});
+legend([l1, l2, l3], {'TD', 'ASD', 'Simulation'});
+
+%% Search procedure
+para_woFB = fmincon(@(x) lossFunc(x, 0.59, 12.41, 0.0), 0.5, [], [], [], [], 0, 1);
+para_wFB1 = fmincon(@(x) lossFunc(x, 0.55, 12.72, 0.0), 0.5, [], [], [], [], 0, 1);
+para_wFB2 = fmincon(@(x) lossFunc(x, 0.51, 14.80, 0.1), 0.5, [], [], [], [], 0, 1);
+
+figure(); subplot(1, 2, 1); hold on;
+errorbar(0.8, mean(scale_woFB_td, 2), std(scale_woFB_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(2.0, mean(scale_wFB1_td, 2), std(scale_wFB1_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(3.2, mean(scale_wFB2_td, 2), std(scale_wFB2_td, 0, 2), '--ob', 'LineWidth', 2);
+
+l1 = plot([0.8, 2, 3.2], mean([scale_woFB_td; scale_wFB1_td; scale_wFB2_td], 2), '--b', 'LineWidth', 2);
+
+errorbar(0.8, mean(scale_woFB_asd, 2), std(scale_woFB_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(2.0, mean(scale_wFB1_asd, 2), std(scale_wFB1_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(3.2, mean(scale_wFB2_asd, 2), std(scale_wFB2_asd, 0, 2), '--or', 'LineWidth', 2);
+
+l2 = plot([0.8, 2, 3.2], mean([scale_woFB_asd; scale_wFB1_asd; scale_wFB2_asd], 2), '--r', 'LineWidth', 2);
+
+l3 = plot([0.8, 2, 3.2], [para_woFB; para_wFB1; para_wFB2], '-ok', 'LineWidth', 2);
+
+xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]); ylim([0.25, 0.8]);
+xticklabels({'woFB', 'wFB1', 'wFB2'});
+legend([l1, l2, l3], {'TD', 'ASD', 'Simulation'});
+
+subplot(1, 2, 2); hold on;
+
+errorbar(0.8, mean(noise_woFB_td, 2), std(noise_woFB_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(2.0, mean(noise_wFB1_td, 2), std(noise_wFB1_td, 0, 2), '--ob', 'LineWidth', 2);
+errorbar(3.2, mean(noise_wFB2_td, 2), std(noise_wFB2_td, 0, 2), '--ob', 'LineWidth', 2);
+
+l1 = plot([0.8, 2, 3.2], mean([noise_woFB_td; noise_wFB1_td; noise_wFB2_td], 2), '--b', 'LineWidth', 2);
+
+errorbar(0.8, mean(noise_woFB_asd, 2), std(noise_woFB_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(2.0, mean(noise_wFB1_asd, 2), std(noise_wFB1_asd, 0, 2), '--or', 'LineWidth', 2);
+errorbar(3.2, mean(noise_wFB2_asd, 2), std(noise_wFB2_asd, 0, 2), '--or', 'LineWidth', 2);
+
+l2 = plot([0.8, 2, 3.2], mean([noise_woFB_asd; noise_wFB1_asd; noise_wFB2_asd], 2), '--r', 'LineWidth', 2);
+
+l3 = plot([0.8, 2, 3.2], [12.41, 12.72, 14.80], '-ok', 'LineWidth', 2);
 
 xlim([0.5, 3.5]); xticks([0.8, 2, 3.2]);
 xticklabels({'woFB', 'wFB1', 'wFB2'});

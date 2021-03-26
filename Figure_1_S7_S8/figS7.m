772db55f0bacc89373cfa193328d6ddca33a17dc
lingqiz <zlqzcc@gmail.com>
2021-03-26 00:05:53 -0400

    minor update

--- Figure_1_S7_S8/figS7.m
+++ Figure_1_S7_S8/figS7.m
@@ -1,14 +1,10 @@
 %% SETUP
 stepSize = 0.01; 
 stmSpc = 0 : stepSize : 2 * pi;
-prior = priorHandle(0.7);
-
-figure();
-plot(stmSpc, prior(stmSpc), '-k'); hold on;
+prior = priorHandle(0.6);
 
 natural = 2 - abs(sin(stmSpc));
 natural = natural / trapz(stmSpc, natural);
-plot(stmSpc, natural, '--k'); hold on;
 
 FI = 15 * prior(stmSpc);
 
@@ -36,35 +32,3 @@ for idx = 1:length(addNoise)
     xticks([0, 45, 90, 135, 180]);
     set(gca,'TickDir','out');
 end
-
-%% PARA
-stepSize = 0.01; 
-stmSpc = 0 : stepSize : 2 * pi;
-prior = priorHandle(0.8);
-
-FI = 15 * prior(stmSpc);
-estVar = 1 ./ FI;
-
-angle = stmSpc / (2 * pi) * 180;
-figure();
-subplot(1, 2, 1);
-plot(angle, prior(stmSpc), 'k', 'LineWidth', 2);
-set(gca,'TickDir','out');
-xticks([0, 45, 90, 135, 180]);
-
-subplot(1, 2, 2);
-plot(angle, FI, 'k', 'LineWidth', 2);
-trapz(stmSpc, FI)
-
-noiseVar = estVar + 0.15 * mean(estVar);
-noiseFI = 1 ./ noiseVar;
-noisePrior = noiseFI ./ trapz(stmSpc, noiseFI);
-
-subplot(1, 2, 1); hold on;
-plot(angle, noisePrior, '--k', 'LineWidth', 2);
-set(gca,'TickDir','out');
-xticks([0, 45, 90, 135, 180]);
-
-subplot(1, 2, 2); hold on;
-plot(angle, noiseFI, '--k', 'LineWidth', 2);
-trapz(stmSpc, noiseFI)
